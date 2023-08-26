e1ff19c71e1a74e27fdbbbf179a4efff8e8f3232
Your Name <tzyman3@gmail.com>
2023-08-26 11:18:45 +0700

    tes

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container : {
+      center : true,
+      padding : '20px',
+    },
+    extend: {
+      colors : {
+        primary : '#14b8a6',
+        secondary : '#64748b',
+        dark : ' #0f172a',
+      },
+    screens : {
+      '2xl' : '1320px',
+      'phone' : '340px',
+      'min-phone' : '240px',
+    },
+    },
+  },
+  plugins: [],
+}
+
